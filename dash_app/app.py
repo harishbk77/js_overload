aa100127605b042d0dea7c0ff9a20d0cff626c87
bellissima0419 <myrealgeorgia@gmail.com>
2019-06-18 23:32:55 -0400

    initial commit

---
+++ dash_app/app.py
@@ -0,0 +1,37 @@
+import os
+
+import pandas as pd
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+#################################################
+# Database Setup
+#################################################
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/js_overload.sqlite"
+db = SQLAlchemy(app)
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(db.engine, reflect=True)
+
+# Save references to each table
+# @TODO create variables to hold the table names
+
+@app.route("/")
+def index():
+    """Return the homepage."""
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run()
---
+++ run.sh
@@ -0,0 +1,1 @@
+FLASK_APP=dash_app/app.py flask run
