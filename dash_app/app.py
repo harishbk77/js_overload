b1e89e2586f1bb2aa0a683d370f83941f6640913
bellissima0419 <myrealgeorgia@gmail.com>
2019-06-24 15:46:43 -0400

    added api routes sqlite3 style

--- dash_app/app.py
+++ dash_app/app.py
@@ -12,26 +12,28 @@ from sqlalchemy import create_engine, func
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+import sqlite3
+import re
+
 app = Flask(__name__)
 
 #################################################
-# Database Setup
+# Database Setup SQLAlchemy
 #################################################
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/js_overload.sqlite"
-db = SQLAlchemy(app)
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/js_overload.sqlite"
+# db = SQLAlchemy(app)
 
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(db.engine, reflect=True)
+# # reflect an existing database into a new model
+# Base = automap_base()
+# # reflect the tables
+# Base.prepare(db.engine, reflect=True)
 
-Survey = Base.classes.jso11k
+# Survey = Base.classes.jso11k
+#################################################
 
 
-# Save references to each table
-# @TODO create variables to hold the table ashshnames
-
+############# sqlite3 ##########################
 @app.route("/")
 def index():
 
@@ -43,13 +45,54 @@ def index():
 def names():
     """Return a list of columns from 11000 rows from the original dataset."""
 
+    conn = sqlite3.connect("dash_app/db/js_overload.sqlite")
+    cur = conn.cursor()
+    cols = '''SELECT sql FROM sqlite_master
+    WHERE tbl_name = `jso11k` AND type = `table`'''
+
+    cur.execute(cols)
+    rows = cur.fetchall()
+    col_string = rows[0][1]
+    pattern = r'`([A-Za-z0-9]*)`'
+    column_names = re.findall(pattern, col_string)[:-1]
+    return jsonify(column_names)
+
+    # column names using sqlalchemy:
+
     # Use Pandas to perform the sql query
-    stmt = db.session.query(Survey).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
+    # stmt = db.session.query(Survey).statement
+    # df = pd.read_sql_query(stmt, db.session.bind)
 
-    # Return a list of the column names (sample names)
-    return jsonify(list(df.columns))
+    # # Return a list of the column names 
+    # return jsonify(list(df.columns))
 
+
+@app.route("/countries")
+def countries():
+    """
+     Return a list of number of survey respondents by country
+    """
+    conn = sqlite3.connect("dash_app/db/js_overload.sqlite")
+    cur = conn.cursor()
+
+    query_string = '''
+        SELECT Country, count(country) FROM jso11k
+        GROUP BY Country
+        ORDER BY COUNT(Country) DESC
+    '''
+
+    cur.execute(query_string)
+    rows = cur.fetchall()
+
+    country_data = []
+
+    for row in rows:
+        tempDict = {}
+        tempDict[row[0]] = int(row[1])
+        country_data.append(tempDict)
+
+
+    return jsonify(country_data)
 
 if __name__ == "__main__":
     app.run()
